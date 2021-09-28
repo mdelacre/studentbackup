a5c74a4681b4033c1c7b4d6a7ebb7ea69a93b1d0
Marie Delacre <marie.delacre@ulb.be>
2021-09-28 09:30:16 +0200

    .

--- Rscripts/Figures/Figure 3.R
+++ Rscripts/Figures/Figure 3.R
@@ -3,43 +3,43 @@
 ####                           AND PRINT IT IN .TXT FILE                           ####        
 #######################################################################################
 
-for (package in "car") {
-    if (!require(package, character.only=T, quietly=T)) {
-        install.packages(package)
-        library(package, character.only=T)
-    }
-}
+#for (package in "car") {
+#    if (!require(package, character.only=T, quietly=T)) {
+#        install.packages(package)
+#        library(package, character.only=T)
+#    }
+#}
 
 setwd(dir="C:/Users/Admin/Documents/Github projects/studentbackup/scripts outputs/Figures/Figure 3/stored files")
            
-simulation=function(n1,n2,sd1,sd2){
+#simulation=function(n1,n2,sd1,sd2){
 
-nSims <- 1000000 #number of simulated experiments (large numbers might take a while)
-p1 <-numeric(nSims) #set up empty container for all simulated Student's t-test p-values 
-p2 <-numeric(nSims) #set up empty container for all simulated Welch's t-test p-values
-p3 <-numeric(nSims) #set up empty container for all simulated Yuen's t-test p-values
+#nSims <- 1000000 #number of simulated experiments (large numbers might take a while)
+#p1 <-numeric(nSims) #set up empty container for all simulated Student's t-test p-values 
+#p2 <-numeric(nSims) #set up empty container for all simulated Welch's t-test p-values
+#p3 <-numeric(nSims) #set up empty container for all simulated Yuen's t-test p-values
 
 #run simulations
 
-for(i in 1:nSims){ #for each simulated experiment
-  sim_x<-rnorm(n = n1, sd = sd1) #simulate participants condition x, as a default option, mean = 0
-  sim_y<-rnorm(n = n2, sd = sd2) #simulate participants condition y, as a default option, mean = 0
+#for(i in 1:nSims){ #for each simulated experiment
+#  sim_x<-rnorm(n = n1, sd = sd1) #simulate participants condition x, as a default option, mean = 0
+#  sim_y<-rnorm(n = n2, sd = sd2) #simulate participants condition y, as a default option, mean = 0
 
   #perform Student, Yuen and Welch t-test
-  p1[i]<-t.test(sim_x,sim_y, alternative = "two.sided", var.equal = TRUE)$p.value #perform the t-test and store p-value
-  p2[i]<-t.test(sim_x,sim_y, alternative = "two.sided", var.equal = FALSE)$p.value #perform the t-test and store p-value
-  p3[i]<-yuen.t.test(sim_x,sim_y, alternative = "two.sided")$p.value #perform the t-test and store p-value
-} 
+#  p1[i]<-t.test(sim_x,sim_y, alternative = "two.sided", var.equal = TRUE)$p.value #perform the t-test and store p-value
+#  p2[i]<-t.test(sim_x,sim_y, alternative = "two.sided", var.equal = FALSE)$p.value #perform the t-test and store p-value
+#  p3[i]<-yuen.t.test(sim_x,sim_y, alternative = "two.sided")$p.value #perform the t-test and store p-value
+#} 
 
-pvalues <- cbind(p1,p2,p3)
-write.table(pvalues,paste("welch's, student's and Yuen's p-values when n1=",n1,", n2=",n2,"and SDR =",round(sd2/sd1,1),".txt"),sep=";",dec=",")
+#pvalues <- cbind(p1,p2,p3)
+#write.table(pvalues,paste("welch's, student's and Yuen's p-values when n1=",n1,", n2=",n2,"and SDR =",round(sd2/sd1,1),".txt"),sep=";",dec=",")
 
-}
+#}
 
-simulation(60,40,1.84,1.84)
-simulation(50,50,0.92,1.84)
-simulation(40,60,0.92,1.84)
-simulation(40,60,1.84,0.92)
+#simulation(60,40,1.84,1.84)
+#simulation(50,50,0.92,1.84)
+#simulation(40,60,0.92,1.84)
+#simulation(40,60,1.84,0.92)
 
 # simulation(40,60,1.63,1.84)
 # simulation(40,60,1.84,1.63)
@@ -61,8 +61,7 @@ setwd(dir="C:/Users/Admin/Documents/Github projects/studentbackup/scripts output
 
 #FIGURE 3A
 png("Figure3a.png",width=2000,height=2000, res = 300)
-plot(n1_60_n2_40_SDR_1[,1],n1_60_n2_40_SDR_1[,2],ylim=c(0,1),xlim=c(0,1),xlab="p-values from Student's t-test", ylab="p-values from Welch's t-test",col="grey63",
-main=paste("Figure 3a:","\n","n1=60   n2=40   SDR = 1"),cex=0.5, cex.axis=1.3,cex.lab=1.3)
+plot(n1_60_n2_40_SDR_1[,1],n1_60_n2_40_SDR_1[,2],ylim=c(0,1),xlim=c(0,1),xlab=substitute(paste(italic('p-'),"values from Student's ",italic('t-'), "test")), ylab=substitute(paste(italic('p-'),"values from Welch's ",italic('t-'), "test")),col="grey63",cex=0.5, cex.axis=1.3,cex.lab=1.3)
 abline(h=0.05) #0.05 line for Welch's test
 abline(v=0.05) #0.05 line for Student's t 
 abline(0,1, col="black", lwd=2) #draw diagonal red line
@@ -70,8 +69,7 @@ dev.off()
 
 #FIGURE 3B
 png("Figure3b.png",width=2000,height=2000, res = 300)
-plot(n1_50_n2_50_SDR_2[,1],n1_50_n2_50_SDR_2[,2],ylim=c(0,1),xlim=c(0,1),xlab="p-values from Student's t-test", ylab="p-values from Welch's t-test",col="grey63",
-main=paste("Figure 3b:","\n","n1=n2=50   SDR = 2"),cex=0.5, cex.axis=1.3,cex.lab=1.3)
+plot(n1_50_n2_50_SDR_2[,1],n1_50_n2_50_SDR_2[,2],ylim=c(0,1),xlim=c(0,1),xlab=substitute(paste(italic('p-'),"values from Student's ",italic('t-'), "test")), ylab=substitute(paste(italic('p-'),"values from Welch's ",italic('t-'), "test")),col="grey63",cex=0.5, cex.axis=1.3,cex.lab=1.3)
 abline(h=0.05) #0.05 line for Welch's test
 abline(v=0.05) #0.05 line for Student's t 
 abline(0,1, col="black", lwd=2) #draw diagonal red line
@@ -79,8 +77,7 @@ dev.off()
 
 #FIGURE 3C
 png("Figure3c.png",width=2000,height=2000, res = 300)
-plot(n1_40_n2_60_SDR_2[,1],n1_40_n2_60_SDR_2[,2],ylim=c(0,1),xlim=c(0,1),xlab="p-values from Student's t-test", ylab="p-values from Welch's t-test",col="grey63",
-main=paste("Figure 3c:","\n","n1=40   n2=60   SDR = 2"),cex=0.5, cex.axis=1.3,cex.lab=1.3)
+plot(n1_40_n2_60_SDR_2[,1],n1_40_n2_60_SDR_2[,2],ylim=c(0,1),xlim=c(0,1),xlab=substitute(paste(italic('p-'),"values from Student's ",italic('t-'), "test")), ylab=substitute(paste(italic('p-'),"values from Welch's ",italic('t-'), "test")),col="grey63",cex=0.5, cex.axis=1.3,cex.lab=1.3)
 abline(h=0.05) #0.05 line for Welch's test
 abline(v=0.05) #0.05 line for Student's t 
 abline(0,1, col="black", lwd=2) #draw diagonal red line
@@ -88,8 +85,7 @@ dev.off()
 
 #FIGURE 3D
 png("Figure3d.png",width=2000,height=2000, res = 300)
-plot(n1_40_n2_60_SDR_0.5[,1],n1_40_n2_60_SDR_0.5[,2],ylim=c(0,1),xlim=c(0,1),xlab="p-values from Student's t-test", ylab="p-values from Welch's t-test",col="grey63",
-main=paste("Figure 3d:","\n","n1=40   n2=60   SDR = 0.5"),cex=0.5, cex.axis=1.3,cex.lab=1.3)
+plot(n1_40_n2_60_SDR_0.5[,1],n1_40_n2_60_SDR_0.5[,2],ylim=c(0,1),xlim=c(0,1),xlab=substitute(paste(italic('p-'),"values from Student's ",italic('t-'), "test")), ylab=substitute(paste(italic('p-'),"values from Welch's ",italic('t-'), "test")),col="grey63",cex=0.5, cex.axis=1.3,cex.lab=1.3)
 abline(h=0.05) #0.05 line for Welch's test
 abline(v=0.05) #0.05 line for Student's t 
 abline(0,1, col="black", lwd=2) #draw diagonal red line
